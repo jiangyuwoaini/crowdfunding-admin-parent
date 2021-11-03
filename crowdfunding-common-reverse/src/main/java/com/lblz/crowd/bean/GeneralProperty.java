907a3030dfa38aa219f243ad17df679a44f8b379
e <13523024606@163.com>
2021-11-03 15:03:56 +0800

    登陆页面提交

---
+++ crowdfunding-common-reverse/src/main/java/com/lblz/crowd/bean/GeneralProperty.java
@@ -0,0 +1,122 @@
+package com.lblz.crowd.bean;
+
+import com.baomidou.mybatisplus.annotation.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @author lblz
+ * @deacription 共用的属性
+ * @date 2021/11/2 9:48
+ **/
+//public class GeneralProperty<T extends Model> extends Model<T> implements Serializable {
+public class GeneralProperty implements Serializable {
+    /**
+     *
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    /**
+     * 创建人id
+     */
+    private Long createId;
+    /**
+     * 修改人id
+     */
+    private Long modifyId;
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+    /**
+     * 修改信息
+     */
+    @TableField(fill = FieldFill.UPDATE)
+    private LocalDateTime updateTime;
+    /**
+     * 删除状态
+     */
+    @TableLogic(delval = "1",value = "0")
+    @TableField(fill = FieldFill.INSERT)
+    private Boolean deleted;
+    /**
+     * 系统id
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Long systemId;
+
+    protected Serializable pkVal() { //该方法表示返回当前id
+        return this.id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCreateId() {
+        return createId;
+    }
+
+    public void setCreateId(Long createId) {
+        this.createId = createId;
+    }
+
+    public Long getModifyId() {
+        return modifyId;
+    }
+
+    public void setModifyId(Long modifyId) {
+        this.modifyId = modifyId;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public Long getSystemId() {
+        return systemId;
+    }
+
+    public void setSystemId(Long systemId) {
+        this.systemId = systemId;
+    }
+
+    @Override
+    public String toString() {
+        return "GeneralProperty{" +
+                "id=" + id +
+                ", createId=" + createId +
+                ", modifyId=" + modifyId +
+                ", createTime=" + createTime +
+                ", updateTime=" + updateTime +
+                ", deleted=" + deleted +
+                ", systemId=" + systemId +
+                '}';
+    }
+}
--- crowdfunding-common-reverse/src/test/java/GeneratorMybatisPlus.java
+++ crowdfunding-common-reverse/src/test/java/GeneratorMybatisPlus.java
@@ -1,71 +1,143 @@
-import org.junit.Test;
 
-import com.baomidou.mybatisplus.enums.IdType;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
-import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
-import com.baomidou.mybatisplus.generator.config.GlobalConfig;
-import com.baomidou.mybatisplus.generator.config.PackageConfig;
-import com.baomidou.mybatisplus.generator.config.StrategyConfig;
-import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class GeneratorMybatisPlus {
-	
-	
-	/**
-	 * 代码生成    示例代码
-	 */
-	@Test
-	public void testGenerator() {
-		//1. 全局配置
-		GlobalConfig config = new GlobalConfig();
-		config.setActiveRecord(true) // 是否支持AR模式
-			  .setAuthor("lblz") // 作者
-			  .setOutputDir("D:\\Program Files\\IntelliJ IDEA 2020.2.2\\projectPath\\crowdfunding-admin-parent\\crowdfunding-common-reverse\\src\\main\\java") // 生成路径
-			  .setFileOverride(true)  // 文件覆盖
-			  .setIdType(IdType.AUTO) // 主键策略
-			  .setServiceName("I%sService")  // 设置生成的service接口的名字的首字母是否为I
-			  					   // IEmployeeService
- 			  .setBaseResultMap(true)
- 			  .setBaseColumnList(true);
-		
-		//2. 数据源配置
-		DataSourceConfig  dsConfig  = new DataSourceConfig();
-		dsConfig.setDbType(DbType.MYSQL)  // 设置数据库类型
-				.setDriverName("com.mysql.jdbc.Driver")
-				.setUrl("jdbc:mysql://localhost:3306/crowd_funding")
-				.setUsername("root")
-				.setPassword("jy321");
-		 
-		//3. 策略配置
-		StrategyConfig stConfig = new StrategyConfig();
-		stConfig.setCapitalMode(true) //全局大写命名
-				.setDbColumnUnderline(true)  // 指定表名 字段名是否使用下划线
-				.setNaming(NamingStrategy.underline_to_camel) // 数据库表映射到实体的命名策略
-//				.setTablePrefix("s_")
-				.setInclude("t_admin");  // 生成的表
-		
-		//4. 包名策略配置 
-		PackageConfig pkConfig = new PackageConfig();
-		pkConfig.setParent("com.lblz.crowd")
-				.setMapper("mapper")
-				.setService("service")
-				.setController("controller")
-				.setEntity("beans")
-				.setXml("mapper");
-		
-		//5. 整合配置
-		AutoGenerator  ag = new AutoGenerator();
-		
-		ag.setGlobalConfig(config)
-		  .setDataSource(dsConfig)
-		  .setStrategy(stConfig)
-		  .setPackageInfo(pkConfig);
-		
-		//6. 执行
-		ag.execute();
-	}	
-	
-	
-	
+
+    /**
+     * <p>
+     * 读取控制台内容
+     * </p>
+     */
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + "：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotBlank(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+
+
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // =====================================全局配置================================================
+        GlobalConfig gc = new GlobalConfig();
+        final String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/crowdfunding-common-reverse/src/main/java");
+        gc.setAuthor("lblz");
+        gc.setOpen(false); //是否打开输出目录
+        gc.setIdType(IdType.AUTO); //指定主键生成策略
+        gc.setDateType(DateType.TIME_PACK); //时间类型 用jdk8的时间
+//        gc.setSwagger2(true);  //实体属性 Swagger2 注解
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://localhost:3306/crowd_funding?useUnicode=true&useSSL=false&characterEncoding=utf8");
+        // dsc.setSchemaName("public");
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("jy321");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        final PackageConfig pc = new PackageConfig();
+//        pc.setModuleName(scanner("模块名"));
+        pc.setParent("com.lblz.crowd");
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        // 如果模板引擎是 freemarker
+        String templatePath = "/templates/mapper.xml.ftl";
+        // 如果模板引擎是 velocity
+        // String templatePath = "/templates/mapper.xml.vm";
+
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
+                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        /*
+        cfg.setFileCreate(new IFileCreate() {
+            @Override
+            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
+                // 判断自定义文件夹是否需要创建
+                checkDir("调用默认方法创建的目录，自定义目录用");
+                if (fileType == FileType.MAPPER) {
+                    // 已经生成 mapper 文件判断存在，不想重新生成返回 false
+                    return !new File(filePath).exists();
+                }
+                // 允许生成模板文件
+                return true;
+            }
+        });
+        */
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+
+        // 配置自定义输出模板
+        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+        // templateConfig.setEntity("templates/entity2.java");
+        // templateConfig.setService();
+        // templateConfig.setController();
+
+        templateConfig.setXml(null);
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setSuperEntityClass("com.lblz.bean.GeneralProperty");
+        strategy.setEntityLombokModel(true); //是否为lombok模型
+        strategy.setRestControllerStyle(true);
+        strategy.setEntityBooleanColumnRemoveIsPrefix(true); //实体类字段是否移除is
+        // 公共父类
+//        strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
+        // 写于父类中的公共字段
+        strategy.setSuperEntityColumns("id");
+        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
 }
